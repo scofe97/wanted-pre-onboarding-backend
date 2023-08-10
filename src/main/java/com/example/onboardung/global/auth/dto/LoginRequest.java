515be18c477ea4de7284947c39b1bbec3a691c45
scofe97 <tscofet@gmail.com>
2023-08-10 17:05:08 +0900

    fix: 로그인 예외 메시지 한글로 변경

--- src/main/java/com/example/onboardung/global/auth/dto/LoginRequest.java
+++ src/main/java/com/example/onboardung/global/auth/dto/LoginRequest.java
@@ -1,15 +1,16 @@
 package com.example.onboardung.global.auth.dto;
 
+import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
 import lombok.Builder;
 
 @Builder
 public record LoginRequest(
-        @Pattern(regexp = ".*@.*", message = "Email should contain @")
+        @Pattern(regexp = ".*@.*", message = "이메일은 @가 포함되어야 합니다.")
         String email,
 
-        @Size(min=8, message="Password should be at least 8 characters")
+        @Size(min=8, message="비밀번호는 최소 8자리 이상이어야 합니다.")
         String password
 ) {
 }
