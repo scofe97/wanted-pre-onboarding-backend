d747699ccd5caa354a627cc290b654648acf5c66
scofe97 <tscofet@gmail.com>
2023-08-10 20:26:45 +0900

    feat: 배포환경 https swagger 추가

--- src/main/java/com/example/onboardung/global/config/SwaggerConfig.java
+++ src/main/java/com/example/onboardung/global/config/SwaggerConfig.java
@@ -1,7 +1,11 @@
 package com.example.onboardung.global.config;
 
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeIn;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
 import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
 import io.swagger.v3.oas.annotations.servers.Server;
 import lombok.RequiredArgsConstructor;
 import org.springdoc.core.models.GroupedOpenApi;
@@ -12,9 +16,16 @@ import org.springframework.context.annotation.Profile;
 @Profile("prod")
 @OpenAPIDefinition(
         servers = @Server(url = "https://wantedonboarding.duckdns.org/"),
-        info = @Info(title = "Couple App",
-                description = "couple app api명세",
-                version = "v1"))
+        info = @Info(title = "Couple App", description = "couple app api명세", version = "v1"),
+        security = @SecurityRequirement(name = "Bearer"))
+@SecurityScheme(
+        name = "Bearer",
+        type = SecuritySchemeType.HTTP,
+        scheme = "bearer",
+        bearerFormat = "JWT",
+        in = SecuritySchemeIn.HEADER,
+        paramName = "Authorization"
+)
 @RequiredArgsConstructor
 @Configuration
 public class SwaggerConfig {
